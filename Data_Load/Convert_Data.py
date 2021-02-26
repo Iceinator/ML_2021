d679590ede4aff976f3c6ce1b98bf8939fff5c52
Nicklas Rasmussen <ncr@hotmail.dk>
2021-02-26 12:50:27 +0100

    Ny data load til standardform

---
+++ Data_Load/Convert_Data.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 22 16:49:38 2021
+
+@author: Nicklas Rasmussen
+"""
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+path = Path(__file__).parent / "../Data_Load/LA_Ozone.csv"
+with path.open() as f:
+    df = pd.read_csv(f)
+
+
+#% Load data
+
+raw_data = df.values
+
+cols = range(0, 10) 
+X = np.array(list(raw_data[:, cols]),dtype=np.float)
+
+# As many of the units in the data is given in the imperial system, the header is first changed to also include units. 
+# Later, the data is converted to the metric system
+attributeNames = np.asarray(df.columns[cols])
+attributeNames = ['Ozone [ppm]','Vandenberg height [m]', 'Wind speed [m/s]','Humidity [%]','Temperature [C]','Inverse Base Temperaure [C]','Dpg [mm Hg]','Inverse Base Height [m]','Visibility [m]','doy [days]']
+
+# Convert the variables to the metric system.
+
+# Temperature (from Fahrenheit to Celcius)
+# Rounds op to nearest integer, as the data frame X 
+# automatically rounds up (and does not do it right by default)
+Celcius_t = np.round((X[:,4]-32)*(5/9),1)
+X[:,4] = Celcius_t
+
+# Wind speed (from mph to m/s)
+windspeed = np.round(X[:,2]*0.44704,1)
+X[:,2] = windspeed
+
+# Visibility (from miles to meters in order to minimize
+# the magnitude of the rounding)
+visibility = np.round(X[:,8]*1609.34,1)
+X[:,8] = visibility
+
+# Inversion Base Height (from feet to m)
+ibh = np.round(X[:,5]*0.3048,1)
+X[:,5] = ibh
+
+# Inversion base temperature (from Fahrenheit to Celcius)
+Celcius_ibt = np.round((X[:,7]-32)*(5/9),1)
+X[:,7] = Celcius_ibt
+
+#%% The data is now converted. Adds the attributeNames and saves as a new csv-file
+metric_data = np.vstack((attributeNames,X) )
+df = pd.DataFrame(metric_data, columns = attributeNames)
+pd.DataFrame(X).to_csv('LA_Ozone_metric.csv')
+
--- Data_Load/Load_Data.py
+++ Data_Load/Load_Data.py
@@ -1,77 +1,69 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Feb 22 16:49:38 2021
+Created on Fri Feb 26 12:41:49 2021
 
 @author: Nicklas Rasmussen
 """
-
+# exercise 1.5.1
 import numpy as np
 import pandas as pd
 from pathlib import Path
 
-path = Path(__file__).parent / "../Data_Load/LA_Ozone.csv"
+path = Path(__file__).parent / "../Data/LA_Ozone_metric.csv"
 with path.open() as f:
     df = pd.read_csv(f)
 
-#%% Load data
-
-# Load the Iris csv data using the Pandas library
-#df = pd.read_csv(f)
-
-# Pandas returns a dataframe, (df) which could be used for handling the data.
-# We will however convert the dataframe to numpy arrays for this course as 
-# is also described in the table in the exercise
-
+#% Load data
 raw_data = df.values
-# Notice that raw_data both contains the information we want to store in an array
-# X (the sepal and petal dimensions) and the information that we wish to store 
-# in y (the class labels, that is the iris species).
 
-# We start by making the data matrix X by indexing into data.
-# We know that the attributes are stored in the four columns from inspecting 
-# the file.
 cols = range(0, 10) 
 X = np.array(list(raw_data[:, cols]),dtype=np.float)
 
-# We can extract the attribute names that came from the header of the csv
+# As many of the units in the data is given in the imperial system, the header is first changed to also include units. 
+# Later, the data is converted to the metric system
 attributeNames = np.asarray(df.columns[cols])
-attributeNames = ['Ozone [ppm]','Vandenberg height [m]', 'Wind speed [m/s]','Humidity [%]','Temperature [C]','Inverse Base Temperaure [C]','Dpg [mm Hg]','Inverse Base Height [m]','Visibility [m]','doy [days]']
-# We can determine the number of data objects and number of attributes using 
-# the shape of X
-N, M = X.shape
 
-# Have to convert the variables to the metric system.
 
-# Temperature (from Fahrenheit to Celcius)
-# Rounds op to nearest integer, as the data frame X 
-# automatically rounds up (and does not do it right by default)
-Celcius_t = np.round((X[:,4]-32)*(5/9),1)
-X[:,4] = Celcius_t
+#%% Unfinished business....
 
-# Wind speed (from mph to m/s)
-windspeed = np.round(X[:,2]*0.44704,1)
-X[:,2] = windspeed
+# Before we can store the class index, we need to convert the strings that
+# specify the class of a given object to a numerical value. We start by 
+# extracting the strings for each sample from the raw data loaded from the csv:
+classLabels = raw_data[:,-1] # -1 takes the last column
+# Then determine which classes are in the data by finding the set of 
+# unique class labels 
+classNames = np.unique(classLabels)
+# We can assign each type of Iris class with a number by making a
+# Python dictionary as so:
+classDict = dict(zip(classNames,range(len(classNames))))
+# The function zip simply "zips" togetter the classNames with an integer,
+# like a zipper on a jacket. 
+# For instance, you could zip a list ['A', 'B', 'C'] with ['D', 'E', 'F'] to
+# get the pairs ('A','D'), ('B', 'E'), and ('C', 'F'). 
+# A Python dictionary is a data object that stores pairs of a key with a value. 
+# This means that when you call a dictionary with a given key, you 
+# get the stored corresponding value. Try highlighting classDict and press F9.
+# You'll see that the first (key, value)-pair is ('Iris-setosa', 0). 
+# If you look up in the dictionary classDict with the value 'Iris-setosa', 
+# you will get the value 0. Try it with classDict['Iris-setosa']
 
-# Visibility (from miles to meters in order to minimize
-# the magnitude of the rounding)
-visibility = np.round(X[:,8]*1609.34,1)
-X[:,8] = visibility
+# With the dictionary, we can look up each data objects class label (the string)
+# in the dictionary, and determine which numerical value that object is 
+# assigned. This is the class index vector y:
+y = np.array([classDict[cl] for cl in classLabels])
+# In the above, we have used the concept of "list comprehension", which
+# is a compact way of performing some operations on a list or array.
+# You could read the line  "For each class label (cl) in the array of 
+# class labels (classLabels), use the class label (cl) as the key and look up
+# in the class dictionary (classDict). Store the result for each class label
+# as an element in a list (because of the brackets []). Finally, convert the 
+# list to a numpy array". 
+# Try running this to get a feel for the operation: 
+# list = [0,1,2]
+# new_list = [element+10 for element in list]
 
-# Inversion Base Height (from feet to m)
-ibh = np.round(X[:,5]*0.3048,1)
-X[:,5] = ibh
 
-# Inversion base temperature (from Fahrenheit to Celcius)
-Celcius_ibt = np.round((X[:,7]-32)*(5/9),1)
-X[:,7] = Celcius_ibt
 
-metric_data = np.vstack((attributeNames,X) )
-pd.DataFrame(X).to_csv('LA_Ozone_metric.csv')
-#np.savetxt("LA_Ozone_metric.csv",X,delimiter=",")
-
-#%% Vi kan overveje at opdele i tyk, mellem og tynd - hvis ikke, så er denne ligegyldig
 # Finally, the last variable that we need to have the dataset in the 
 # "standard representation" for the course, is the number of classes, C:
 C = len(classNames)
-
-#%% Identificér brugbar/ikke brugbar data
