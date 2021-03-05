0ad932f6449570bf1ed79b1d01b61b3a07cd457d
Nicklas Rasmussen <ncr@hotmail.dk>
2021-03-05 10:03:10 +0100

    Oulier/Histogram done

--- Data_Load/Attribute_variance_and_distribution.py
+++ Data_Load/Attribute_variance_and_distribution.py
@@ -28,6 +28,7 @@ title('Wine: Boxplot (standarized)')
 boxplot(zscore(X, ddof=1))
 xticks(range(1,M+1), attributeNames, rotation=45)
 
+# Histogram for all attributes
 figure(figsize=(14,9))
 u = np.floor(np.sqrt(M)); 
 v = np.ceil(float(M)/u)
@@ -39,115 +40,16 @@ for i in range(M):
     if i%v!=0: yticks([])
     if i==0: title('Ozone: Histogram')
 
+# Historgram with outlier attributes
+figure(figsize=(14,9))
+m = [0, 1, -1]
+for i in range(len(m)):
+    subplot(1,len(m),i+1)
+    hist(X[:,m[i]],50)
+    xlabel(attributeNames[m[i]])
+    ylim(0, N) # Make the y-axes equal for improved readability
+    if i>0: yticks([])
+    if i==0: title('Ozone: Histogram (Vandenberg Height, Wind speed & Visibility)')
 
+# No concrete outliers
 
-#%%
-bins = 20
-
-plt.subplot(1,2,1)
-plt.hist(X[:,0],bins)
-plt.title('Vandenberg height')
-plt.xlabel('Height [m]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,0])
-plt.title('Boxplot of Vandenberg Height')
-
-plt.show()
-
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,1],bins)
-plt.title('Wind speed')
-plt.xlabel('Wind speed [m/s]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,1])
-plt.title('Boxplot of Wind speed')
-
-plt.show()
-
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,2],bins)
-plt.title('Humidity')
-plt.xlabel('Humidity [%]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,2])
-plt.title('Boxplot of Humidity')
-
-plt.show()
-
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,3],bins)
-plt.title('Temperature')
-plt.xlabel('Temperature [C]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,3])
-plt.title('Boxplot of Temperature')
-
-plt.show()
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,4],bins)
-plt.title('Inverse Base Temperature [C]')
-plt.xlabel('Tempearture [C]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,4])
-plt.title('Boxplot of IBT')
-
-plt.show()
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,5],bins)
-plt.title('Dpg')
-plt.xlabel('Dpg [mmHg]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,5])
-plt.title('Boxplot of Dpg')
-
-plt.show()
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,6],bins)
-plt.title('Inverse Base Height')
-plt.xlabel('Height [m]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,6])
-plt.title('Boxplot of IBH')
-
-plt.show()
-
-
-plt.subplot(1,2,1)
-plt.hist(X[:,7],bins)
-plt.title('Visibility')
-plt.xlabel('Distance [km]')
-plt.ylabel('Frequency')
-
-plt.subplot(1,2,2)
-plt.boxplot(X[:,7])
-plt.title('Boxplot of Dpg')
-
-plt.show()
-
-# It is clear that the "Visibility" attribute
